740373e5329513fe90f1f8c184376beeca7b3824
NanoProgram <danielj.perezd@gmail.com>
2022-11-21 20:38:27 +0000

    Incio de ejercicio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["The", "Our"];
+let adj = ["Great", "Big"];
+let noun = ["Jogger", "Racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  console.log(pronoun[i]);
+}
+
+for (let i = 0; i < adj.length; i++) {
+  console.log(adj[i]);
+}
+for (let i = 0; i < noun.length; i++) {
+  console.log(noun[i]);
+}
